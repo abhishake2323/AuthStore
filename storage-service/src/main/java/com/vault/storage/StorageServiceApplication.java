f907158b5d2a6eeb701854234bebcaac63e7fb5e
abhishake2323 <abhishakem@hotmail.com>
2026-04-09 02:33:30 -0400

    Configure virtual threads and token validation

---
+++ storage-service/src/main/java/com/vault/storage/StorageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vault.storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class StorageServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StorageServiceApplication.class, args);
+    }
+}
